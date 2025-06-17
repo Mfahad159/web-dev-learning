b0e9b0d4401b21d59140d47a6fc4df4dff962cb3
Mfahad159 <mianfahad664@gmail.com>
2025-06-17 21:17:31 +0500

    update:added a button using listener Dom-Project

--- yt-cards-DOM-project/card.js
+++ yt-cards-DOM-project/card.js
@@ -1,24 +1,24 @@
-const mainContainer = document.querySelector('.main-container');
-
-// Create new .card element
-const newItem = document.createElement('div');
-newItem.className = 'card';
-newItem.innerHTML = `
-                  <div class="left-section">
-            <img src="images/thumb-1.avif" alt="">
-            <div class="duration">5:00</div>
-        </div>
-
-        <div class="right-section">
-            <div class="vid-title">Video Title</div>
-            <div class="vid-stats"> 
-                <span class="channel-name">CoDicWorld &#183;</span>
-                <span class="views">1.2M views &#183;</span>
-                <span class="date">1 week ago</span>
+ document.body.querySelector('.add-button').addEventListener('click',()=>{
+    const mainContainer = document.querySelector('.main-container');
+    // Create new .card element
+    const newItem = document.createElement('div');
+    newItem.className = 'card';
+    newItem.innerHTML = `
+        <div class="left-section">
+                <img src="images/thumb-1.avif" alt="">
+                <div class="duration">5:00</div>
             </div>
-        </div>
-    `;
 
-// Insert before the first .card (i.e., at the top)
-const firstItem = mainContainer.querySelector('.card');
-mainContainer.insertBefore(newItem, firstItem);
+            <div class="right-section">
+                <div class="vid-title">Video Title</div>
+                <div class="vid-stats">
+                    <span class="channel-name">CoDicWorld &#183;</span>
+                    <span class="views">1.2M views &#183;</span>
+                    <span class="date">1 week ago</span>
+                </div>
+            </div>
+    `;
+    // Insert before the first .item ( at the top)
+    const firstItem = mainContainer.querySelector('.card');
+    mainContainer.insertBefore(newItem, firstItem);
+ });
